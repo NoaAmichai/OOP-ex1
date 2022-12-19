cd712448ac4946df6de0ca1ae6bf40ba0c5fd8dd
Aviost111 <Aviostroff@yahoo.com>
2022-12-19 14:45:52 +0200

    dont be stupid

--- OOP.Assignment1/src/main/java/observer/GroupAdmin.java
+++ OOP.Assignment1/src/main/java/observer/GroupAdmin.java
@@ -20,21 +20,33 @@ public class GroupAdmin implements Sender{
     @Override
     public void insert(int offset, String obj) {
         undoableStringBuilder.insert(offset,obj);
+        for (Member member:this.members) {
+            member.update(undoableStringBuilder);
+        }
     }
 
 
     @Override
     public void append(String obj) {
         undoableStringBuilder.append(obj);
+        for (Member member:this.members) {
+            member.update(undoableStringBuilder);
+        }
     }
 
     @Override
     public void delete(int start, int end) {
         undoableStringBuilder.delete(start, end);
+        for (Member member:this.members) {
+            member.update(undoableStringBuilder);
+        }
     }
 
     @Override
     public void undo() {
         undoableStringBuilder.undo();
+        for (Member member:this.members) {
+            member.update(undoableStringBuilder);
+        }
     }
 }
