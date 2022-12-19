4559b555d0c36224ee3c94c74f3e01ca2aff7c12
Aviost111 <Aviostroff@yahoo.com>
2022-12-19 14:49:14 +0200

    dont be stupid

---
+++ OOP.Assignment1/src/test/java/observer/GroupAdminTest.java
@@ -0,0 +1,32 @@
+package observer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupAdminTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void unregister() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void append() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void undo() {
+    }
+}
