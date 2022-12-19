12e3cf3ebad086a834e927255c1790cea8672f28
Noa Amichai <Noa1254@gmail.com>
2022-12-19 14:24:13 +0200

    Update GroupAdmin.java

--- OOP.Assignment1/src/main/java/observer/GroupAdmin.java
+++ OOP.Assignment1/src/main/java/observer/GroupAdmin.java
@@ -2,21 +2,21 @@ package observer;
 
 import java.util.ArrayList;
 
-public class GroupAdmin implements Sender{
+public class GroupAdmin extends UndoableStringBuilder implements Sender{
+    private ArrayList<Member> members = new ArrayList<>();
 
     @Override
     public void register(Member obj) {
-
+        members.add(obj);
     }
 
     @Override
     public void unregister(Member obj) {
-
+        members.remove(obj);
     }
 
     @Override
     public void insert(int offset, String obj) {
-
     }
 
     @Override
