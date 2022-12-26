9a6baaf64561c203b345a1682b48d3b7c6f4d8e2
Aviost111 <Aviostroff@yahoo.com>
2022-12-26 18:51:36 +0200

    dont be stupid,stammmmmm

--- OOP.Assignment1/src/test/java/observer/ConcreteMemberTest.java
+++ OOP.Assignment1/src/test/java/observer/ConcreteMemberTest.java
@@ -7,30 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConcreteMemberTest {
     ConcreteMember member1 = new ConcreteMember();
     ConcreteMember member2 = new ConcreteMember();
+
     GroupAdmin admin = new GroupAdmin();
 
     @Test
     void update() {
         System.out.println(JvmUtilities.objectFootprint(admin));
+        System.out.println(JvmUtilities.objectFootprint(member1));
+        String concreteSize=JvmUtilities.objectTotalSize(member1);
         admin.register(member1);
+        System.out.println(JvmUtilities.objectFootprint(member1));
         admin.unregister(member1);
-        admin.register(member1);
-        admin.unregister(member1);
-        admin.register(member1);
-        admin.unregister(member1);
-        admin.register(member1);
-        admin.unregister(member1);
-        admin.register(member1);
-        admin.unregister(member1);
-        admin.register(member1);
-        System.out.println(JvmUtilities.objectFootprint(admin));
-        admin.register(member2);
-        System.out.println(JvmUtilities.objectFootprint(admin));
-        admin.unregister(member2);
-        admin.unregister(member1);
-        System.out.println(JvmUtilities.objectTotalSize(admin));
+        assertEquals(JvmUtilities.objectTotalSize(member1),concreteSize);
         admin.append("Check that the member doesn't get updates from the admin");
-//        assertEquals(member1.getInfo().toString(),"");
         admin.register(member1);
         System.out.println("member 1 size");
         System.out.println(JvmUtilities.objectTotalSize(member1));
@@ -42,8 +31,7 @@ class ConcreteMemberTest {
         assertEquals(member1.getInfo(),member2.getInfo());
 
         admin.unregister(member1);
-//        assertNotEquals(member1.getInfo(),member2.getInfo()); //Check that after unregistering the members are different.
-
+        assertNotEquals(member1.getInfo(),member2.getInfo()); //Check that after unregistering the members are different.
         admin.delete(0, 100);
         admin.reverse();
         assertEquals("eta",member2.getInfo().toString());
